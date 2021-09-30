6754b2d6858d26c5e9e8596f5e2c62236e22866c
Leonardo Coelho <leonardohcoelho@gmail.com>
2021-09-30 13:53:36 -0300

    ;)

---
+++ sec1.5.3.esclarecimento2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+sec_1_5_3_esclarecimento_2()
+{
+    int inicio, inicio2, fim;
+
+    if('A' > 'a')
+    {
+        inicio = 'a';
+        inicio2 = 'A';
+        fim = 'z';
+    }
+    else
+    {
+        inicio = 'A';
+        inicio2 = 'a';
+        fim = 'Z';
+    }
+
+    int distancia = inicio2-inicio;
+
+    printf("(PROGRAMA \"TODAS AS LETRAS DA TABELA ASCII\")\n\nLETRA\tCODIGO ASCII\n");
+    for(int c = inicio; c <= fim; c++)
+    {
+        printf("%c\t%d\n", c, c);
+        printf("%c\t%d\n", c+distancia, c+distancia);
+    }
+}
