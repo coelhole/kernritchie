4d7a9bad8bbe3d7248c901c3005abc78d5322858
Leonardo Coelho <leonardohcoelho@gmail.com>
2021-09-30 09:28:17 -0300

    ;)

--- exerc1.2.c
+++ exerc1.2.c
@@ -3,64 +3,64 @@
 prog1_2()
 {
 	printf("printf(\"\\j\\p\"); imprime ");
-    printf("\j\p");
-    printf("\n");
+	printf("\j\p");
+	printf("\n");
 
 	printf("printf(\"\\8\\9\"); imprime ");
-    printf("\8\9");
-    printf("\n");
+	printf("\8\9");
+	printf("\n");
 
 	printf("printf(\"\\1\"); imprime ");
-    printf("\1");
-    printf("\n");
+	printf("\1");
+	printf("\n");
 
 	printf("printf(\"\\2\"); imprime ");
-    printf("\2");
-    printf("\n");
+	printf("\2");
+	printf("\n");
 
 	printf("printf(\"\\1\\2\"); imprime ");
-    printf("\1\2");
-    printf("\n");
+	printf("\1\2");
+	printf("\n");
 
 	printf("printf(\"\\*\"); imprime ");
-    printf("\*");
-    printf("\n");
+	printf("\*");
+	printf("\n");
 
 	printf("printf(\"\\&\"); imprime ");
-    printf("\&");
-    printf("\n");
+	printf("\&");
+	printf("\n");
 
 	printf("printf(\"\\|\"); imprime ");
-    printf("\|");
-    printf("\n");
+	printf("\|");
+	printf("\n");
 
 	printf("printf(\"\\/\"); imprime ");
-    printf("\/");
-    printf("\n");
+	printf("\/");
+	printf("\n");
 
 	printf("printf(\"\\@\"); imprime ");
-    printf("\@");
-    printf("\n");
+	printf("\@");
+	printf("\n");
 
 	printf("printf(\"\\~\"); imprime ");
-    printf("\~");
-    printf("\n");
+	printf("\~");
+	printf("\n");
 
 	printf("printf(\"\\$\"); imprime ");
-    printf("\$");
-    printf("\n");
+	printf("\$");
+	printf("\n");
 
 	printf("printf(\"\\!\"); imprime ");
-    printf("\!");
-    printf("\n");
+	printf("\!");
+	printf("\n");
 
 	printf("printf(\"\\?\"); imprime ");
-    printf("\?");
-    printf("\n");
+	printf("\?");
+	printf("\n");
 
 	printf("printf(\"\\=\"); imprime ");
-    printf("\=");
-    printf("\n");
+	printf("\=");
+	printf("\n");
 
-    printf("teste outros\n");
+	printf("teste outros\n");
 }
