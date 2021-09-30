06069a70e91b9a7ad0b4f85574cf86f91743e000
Leonardo Coelho <leonardohcoelho@gmail.com>
2021-09-30 00:32:04 -0300

    correção

--- sec1.5.esclarecimento2.c
+++ sec1.5.esclarecimento2.c
@@ -7,7 +7,7 @@ sec_1_5_esclarecimento_2()
     while(c = getchar()){   /* como getchar() le do teclado, o caractere NUL ('\0'), que eh 0, nunca sera lido e, portanto, este eh um laco infinito */
         printf("Caractere lido: ");
         putchar(c);
-        putchar("\n");
+        putchar('\n');
         if(c=='\n')
             printf("Escreva algo e tecle <enter> para que a sequencia dos caracteres fornecidos seja lida: ");
     }
