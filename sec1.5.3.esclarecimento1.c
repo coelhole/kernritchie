b55630fe6024ea0fcb06953c80e50f5957a198d9
Leonardo Coelho <leonardohcoelho@gmail.com>
2021-09-30 13:55:29 -0300

    ;)

--- sec1.5.3.esclarecimento1.c
+++ sec1.5.3.esclarecimento1.c
@@ -7,7 +7,7 @@ sec_1_5_3_esclarecimento_1()
 	/* TESTE DE IGUALDADE ERRADO! */
     printf("Valor de c, antes do if, eh igual a %d (que represante o caractere \'%c\').\n", c, c);
     if(c = 'A')
-        printf("Sim, a condicao eh verdadeira! (Eh diferente de 0!)\nc eh \%d e \'A\' eh \%d.\n", c, 'A');
+        printf("Sim, a condicao eh verdadeira! (Eh diferente de 0!)\nc eh %d e \'A\' eh %d.\n", c, 'A');
     printf("Note que o if mudou o valor de c! Por que?\nOra, = eh o operador de atribuicao e nao de igualdade! O operador de igualdade eh ==.\n");
     printf("Primeiro c recebeu o valor numérico de \'A\' (%d) e, apenas depois, verificou se o valor de c era diferente de 0 (isto eh, \"verdadeiro\")\n", 'A');
 	/* ************************* */
