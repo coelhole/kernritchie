4c7d08f9a133ee5f159b26f755bbbbf2ff4f122e
Leonardo Coelho <leonardohcoelho@gmail.com>
2021-09-29 23:31:04 -0300

    exerc1.2

---
+++ exerc1.2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+prog1_2()
+{
+    printf("\j\p");/* imprime jp */
+    printf("\n");
+
+    printf("\8\9");/* imprime 89 */
+    printf("\n");
+
+    printf("\1");/* nao imprime 1 */
+    printf("\n");
+
+    printf("\2");/* nao imprime 2 */
+    printf("\n");
+
+    printf("\1\2");/* nao imprime 12 */
+    printf("\n");
+
+    printf("\*");/* imprime * */
+    printf("\n");
+
+    printf("\&");/* imprime & */
+    printf("\n");
+
+    printf("\|");/* imprime | */
+    printf("\n");
+
+    printf("\/");/* imprime / */
+    printf("\n");
+
+    printf("\@");/* imprime @ */
+    printf("\n");
+
+    printf("\~");/* imprime ~ */
+    printf("\n");
+
+    printf("\$");/* imprime $ */
+    printf("\n");
+
+    printf("\!");/* imprime ! */
+    printf("\n");
+
+    printf("\?");/* imprime ? */
+    printf("\n");
+
+    printf("\=");/* imprime = */
+    printf("\n");
+
+    /* teste outros */
+}
