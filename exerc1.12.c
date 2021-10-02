97fb49fe6e9db4fa3b0ffff39435c4acd48610a7
Leonardo Coelho <leonardohcoelho@gmail.com>
2021-10-01 22:54:42 -0300

    ;;

---
+++ exerc1.12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+prog1_12()
+{
+	int c, canterior;
+	while((c = getchar()) != EOF){
+		if(c != ' ' && c != '\t' && c != '\n')
+			putchar(c);
+        /* ELSE: c == ' ' || c == '\t' || c == '\n' */
+		else if(canterior != ' ' && canterior != '\t' && canterior != '\n') /* uma cadeia composta por ' ', '\t' e '\n' deve produzir uma unica quebra de linha */
+			putchar('\n');
+		canterior = c;
+	}
+}
